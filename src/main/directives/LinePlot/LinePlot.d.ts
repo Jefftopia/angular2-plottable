3eeca90365dd4d60aef4647d93dce6f06b99f5af
jsmith <j.smith@escmatrix.com>
2015-11-24 11:43:33 -0500

    comment *.ts lines for debugging; add tsconfig.json for using tsc dependency for building; update package.json with dependencies;

--- src/main/directives/LinePlot/LinePlot.d.ts
+++ src/main/directives/LinePlot/LinePlot.d.ts
@@ -1,13 +1,12 @@
-/// <reference path="../../../../node_modules/angular2/bundles/typings/angular2/angular2.d.ts" />
 /// <reference path="../../../../bower_components/plottable/plottable.d.ts" />
-import { HostViewRef } from "angular2/angular2";
+import { Host } from "angular2/angular2";
 export declare class LinePlot {
     xScale: Plottable.Scales.Category;
     yScale: Plottable.Scales.Linear;
     plot: Plottable.Plots.Line;
     data: any[];
     dataSet: any;
-    elementRef: HostViewRef;
-    constructor(elementRef: HostViewRef);
+    elementRef: Host;
+    constructor(elementRef: Host);
     render(): void;
 }
--- src/main/index.ts
+++ src/main/index.ts
@@ -3,35 +3,43 @@
 /// <reference path="../../node_modules/angular2/bundles/typings/angular2/router.d.ts" />
 /// <reference path="directives/LinePlot/LinePlot.ts" />
 
-import { Component, View, bootstrap, CORE_DIRECTIVES, provide, ViewChild } from 'angular2/angular2';
-import { LinePlot } from "./directives/LinePlot/LinePlot";
+//import { Component, View, bootstrap, CORE_DIRECTIVES, provide, ViewChild } from 'angular2/angular2';
+//import { LinePlot } from "./directives/LinePlot/LinePlot";
 
-namespace app {
+namespace test {
+
+    export class TestClass {
         
-    @Component({
-        selector: 'plottable-app',
-        directives: [LinePlot],
-        templateUrl: 'PlotRoot.html'
-    })
-//    @RouteConfig([
-//        { path: '/', component: MatchesView, as: 'Matches' },
-//        { path: '/matches', component: MatchesView, as: 'Matches' },
-//        { path: '/match-details:matchId', component: MatchDetailsView, as: 'MatchDetails'}
-//    ])
-    export class RootPlotDemoComponent {
-    
-        linePlot: LinePlot;
-        
-        constructor(linePlot: LinePlot) {
-            
-            this.linePlot = linePlot;
-            
-            console.log('New Root');
-                                    
-        }
-           
     }
-            
+
 }
 
-bootstrap(app.RootPlotDemoComponent, [LinePlot]);
+//namespace app {
+//        
+//    @Component({
+//        selector: 'plottable-app',
+//        directives: [LinePlot],
+//        templateUrl: 'PlotRoot.html'
+//    })
+////    @RouteConfig([
+////        { path: '/', component: MatchesView, as: 'Matches' },
+////        { path: '/matches', component: MatchesView, as: 'Matches' },
+////        { path: '/match-details:matchId', component: MatchDetailsView, as: 'MatchDetails'}
+////    ])
+//    export class RootPlotDemoComponent {
+//    
+//        linePlot: LinePlot;
+//        
+//        constructor(linePlot: LinePlot) {
+//            
+//            this.linePlot = linePlot;
+//            
+//            console.log('New Root');
+//                                    
+//        }
+//           
+//    }
+//            
+//}
+//
+//bootstrap(app.RootPlotDemoComponent, [LinePlot]);
